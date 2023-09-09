f53c257d526c06fb94d1100042eea8db41ba528c
Rahul Bhatt <rbhatt291@gmail.com>
2023-09-10 01:29:24 +0530

    day3

--- reserveit/src/App.js
+++ reserveit/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Register from './Register';
 import Login from './Login';
-//import Register from './Register';
 
 function App() {
   return (
     <div className="App">
-      <Login></Login>
+      <Register></Register>
     </div>
   );
 }
