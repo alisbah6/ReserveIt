2a92c2aa4424b76bc572b1d36f931ff5c8aad00f
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2023-09-26 14:40:03 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 //import Home from './components/Home';
 //import Navbar from './components/Navbar';
-//import Login from './Login';
-import Register from './Register';
+import Login from './Login';
+//import Register from './Register';
 
 
 function App() {
   return (
     <div className="App">
-      <Register>
-      </Register>
+      <Login/>
     </div>
   );
 }
