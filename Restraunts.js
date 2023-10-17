8b6b2487d74fa3c50c13eecab8e663eabe0babec
alisbah6 <103383751+alisbah6@users.noreply.github.com>
2023-10-17 13:17:40 +0530

    Add files via upload

---
+++ Restraunts.js
@@ -0,0 +1,21 @@
+export const data=[{
+    id:'1',
+    name:"Sagar Ratna",
+    b1:"Defence Colony",
+    b2:"Preet Vihar",
+    b3:"Masjid Moth",
+    b4:"Naraina Vihar",
+    b5:"Paschim Vihar",
+    desc:"This is a description for sagar ratna",
+},
+{
+    id:'2',
+    name:"Sandoz",
+    b1:"Connaught Place",
+    b2:"Karol Bagh",
+    b3:"Lajpat Nagar",
+    b4:"Rajouri Garden",
+    b5:"Jasola",
+    desc:"This is a description for sandoz"
+}
+];
