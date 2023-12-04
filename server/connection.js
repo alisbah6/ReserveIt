a085cd9cda54a1b0cfba582f0636766c0ee52093
alisbah6 <103383751+alisbah6@users.noreply.github.com>
2023-12-04 15:39:56 +0000

    update

--- server/connection.js
+++ server/connection.js
@@ -1,26 +1,16 @@
-// import {mongoose } from 'mongoose';
 const mongoose = require('mongoose');
-const http = require("http");
 const URL = "mongodb+srv://alisbahhina:XLIBbcIE0dsWDsSp@cluster0.rd3md3x.mongodb.net/";
 
-async function connetDb(){
-
+async function connetDb() {
     try {
-        
+
         await mongoose.connect(URL);
         console.log("connected to database");
 
     } catch (error) {
         console.log(error);
     }
-
-    // http.listen(4000, function () {
-    //     console.log("Server is running on Port 4000 ");
-    //   });
 }
 
-
 // export default connetDb;
-
-
 connetDb();
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const http = require("http").Server(app);
 const cors = require("cors");
 
 require("dotenv").config();
 const connectDB = require("./connection");
 
+http.listen(4000, function () {
+    console.log("Server is running on Port 4000 ");
+  });
