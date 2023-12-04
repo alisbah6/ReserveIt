9f45b5774ec477866dfda38abc966128d7618d97
alisbah6 <103383751+alisbah6@users.noreply.github.com>
2023-12-04 14:51:27 +0000

    new

--- server/connection.js
+++ server/connection.js
@@ -1,13 +1,25 @@
-const mongoose = require("mongoose");
+import {mongoose } from 'mongoose';
+const http = require("http").Server(app);
+const URL = "mongodb+srv://alisbahhina:XLIBbcIE0dsWDsSp@cluster0.rd3md3x.mongodb.net/";
 
-const URL =
-  "mongodb+srv://alisbahhina:XLIBbcIE0dsWDsSp@cluster0.rd3md3x.mongodb.net/";
+async function connetDb(){
 
-async function connetDb() {
-  return mongoose.connect(URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    try {
+        
+        await mongoose.connect(URL,{useNewUrlParser:true,useUnifiedTopology:true});
+        console.log("connected to database");
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    http.listen(4000, function () {
+        console.log("Server is running on Port 4000 ");
+      });
 }
 
-module.exports = connetDb;
+
+export default connetDb;
+
+
+connetDb();
--- server/server.js
+++ server/server.js
@@ -1,16 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config();
 const connectDB = require("./connection");
 
-const PORT = 3500;
-
-app.listen(PORT, async () => {
-  try {
-    await connectDB();
-    console.log(`server running on port ${PORT}`);
-  } catch (error) {
-    console.log(error);
-  }
-});
