2b6daf3d9a3cb6e10f75b03582e664c5d157b06e
alisbah6 <103383751+alisbah6@users.noreply.github.com>
2023-12-04 15:29:36 +0000

    working

--- server/connection.js
+++ server/connection.js
@@ -1,25 +1,26 @@
-import {mongoose } from 'mongoose';
-const http = require("http").Server(app);
+// import {mongoose } from 'mongoose';
+const mongoose = require('mongoose');
+const http = require("http");
 const URL = "mongodb+srv://alisbahhina:XLIBbcIE0dsWDsSp@cluster0.rd3md3x.mongodb.net/";
 
 async function connetDb(){
 
     try {
         
-        await mongoose.connect(URL,{useNewUrlParser:true,useUnifiedTopology:true});
+        await mongoose.connect(URL);
         console.log("connected to database");
 
     } catch (error) {
         console.log(error);
     }
 
-    http.listen(4000, function () {
-        console.log("Server is running on Port 4000 ");
-      });
+    // http.listen(4000, function () {
+    //     console.log("Server is running on Port 4000 ");
+    //   });
 }
 
 
-export default connetDb;
+// export default connetDb;
 
 
 connetDb();
