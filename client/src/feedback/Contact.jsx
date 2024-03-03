17a7e8b1815330477bc7bea6f875d1bf44965043
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2024-03-03 17:53:06 +0000

    New

--- client/src/feedback/Contact.jsx
+++ client/src/feedback/Contact.jsx
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { useState } from 'react';
 
 function Contact() {
-  const [records,setRecords]=useState([]);
+  const [records, setRecords] = useState([]);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
@@ -14,38 +14,39 @@ function Contact() {
   const submit = async (e) => {
     e.preventDefault();
     try {
-        // Make an API request to create a new user
-        const response = await axios.post(
-            "http://localhost:3500/user/feedback",{
-            name,
-            email,
-            msg,}
-        );
-        if (response.status === 201) {
-            // User registration was successful
-            console.log("Feedback Successfull");
-            console.log("Responded Data",response.data);
-            // Redirect or perform other actions as needed
-        }
+      // Make an API request to create a new user
+      const response = await axios.post(
+        "http://localhost:3500/user/feedback", {
+        name,
+        email,
+        msg,
+      }
+      );
+      if (response.status === 201) {
+        // User registration was successful
+        console.log("Feedback Successfull");
+        console.log("Responded Data", response.data);
+        // Redirect or perform other actions as needed
+      }
     } catch (error) {
-        // Handle registration errors
-        console.error("Error in Feedback:", error);
-    }
-};
-useEffect(()=>{
-  const fetchFeedbackResponses = async () => {
-    try {
-        const response = await axios.get("http://localhost:3500/user/feedbacks");
-        if (response.status === 200) {
-            // Return the array of feedback responses
-            setRecords(response.data);
-            localStorage.setItem('feedbackRecords', JSON.stringify(response.data));
-        }
-    } catch (error) {
-        console.error("Error fetching feedback responses:", error);
+      // Handle registration errors
+      console.error("Error in Feedback:", error);
     }
   };
-  const cachedRecords = localStorage.getItem('feedbackRecords');
+  useEffect(() => {
+    const fetchFeedbackResponses = async () => {
+      try {
+        const response = await axios.get("http://localhost:3500/user/feedbacks");
+        if (response.status === 200) {
+          // Return the array of feedback responses
+          setRecords(response.data);
+          localStorage.setItem('feedbackRecords', JSON.stringify(response.data));
+        }
+      } catch (error) {
+        console.error("Error fetching feedback responses:", error);
+      }
+    };
+    const cachedRecords = localStorage.getItem('feedbackRecords');
     if (cachedRecords) {
       // If cached records exist, parse and set them into state
       setRecords(JSON.parse(cachedRecords));
@@ -53,50 +54,52 @@ useEffect(()=>{
       // If no cached records, fetch them
       fetchFeedbackResponses();
     }
-  const interval=setInterval(()=>{
-     fetchFeedbackResponses()
-      },100)
-  return()=>clearInterval(interval)   
-},[])
+    const interval = setInterval(() => {
+      fetchFeedbackResponses()
+    }, 100)
+    return () => clearInterval(interval)
+  }, [])
 
 
   return (
-    <div className='bg'>
+    <div>
+      <Navbar />
+      <br></br>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       <div className='container-content'>
           <h1 className='heading-contact'>Contact Us</h1>
-          <div className='contact-deatils'>
+          <div className='feed-address'>
             <div className='contactInfo'>
               <div className='box-one'>
-                <h4><i class="fas fa-location"></i>Address</h4>
-                <p>New Delhi 110013</p>
+                <p className='font-user'><i class="fas fa-location"></i> Address</p>
+                <p className='font-comment'>New Delhi 110013</p>
               </div>
               <div className='box-one'>
-                <h4><i className='fa fa-phone'></i>Phone</h4>
-                <p>011 41827498</p>
+                <p className='font-user'><i className='fa fa-phone'></i> Phone</p>
+                <p className='font-comment'>011 41827498</p>
               </div>
               <div className='box-one'>
-                <h4><i class="fa fa-envelope"></i>Email</h4>
-                <p>alisbahhina@gmail.com</p>
+                <p className='font-user'><i class="fa fa-envelope"></i> Email</p>
+                <p className='font-comment'>alisbahhina@gmail.com</p>
               </div>
             </div>
             <div className='feedInfo'>
-              <div className='box-two'>
-                <h4>Send Feedback/Message</h4>
-                <br />
-                <input className='feed' placeholder='Full Name' id="name" onChange={(e)=>{ setName(e.target.value);}} />
-                <br />
-                <input className='feed' placeholder='Email' id="email" onChange={(e)=>{setEmail(e.target.value);}} />
-                <br />
-                <input className='info' placeholder='Type your message' id="msg" onChange={(e)=>{setMsg(e.target.value);}}/>
-                <br />
-                <button className='send' onClick={submit}>Send</button>
-              </div>
+              <h3 className='send-feed'>Send Feedback/Message</h3>
+              <input className='feed' placeholder='Full Name' id="name" onChange={(e) => { setName(e.target.value); }} />
+              <input className='feed' placeholder='Email' id="email" onChange={(e) => { setEmail(e.target.value); }} />
+              <textarea className='info' placeholder='Type your message' id="msg" onChange={(e) => { setMsg(e.target.value); }} />
+              <button className='button_feed' onClick={submit}>Send</button>
             </div>
-          </div>
         </div>
+        <ul className='feedback-comment'>
+        <h1 className='heading-contact'>Feedbacks</h1>
+          {records.map(record => (
+            <li className='comment' key={record.id}>
+              <p className='font-user'><i class="fa-solid fa-circle-user"></i> {record.name}</p>
+              <p className='font-comment'>{record.msg}</p></li>
+          ))}
+        </ul>
       </div>
-      <br />
       <Footer />
     </div>
   );
