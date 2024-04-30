a59d048fa44468a5df208772018e2d10fb104b35
Aman Bhatt <22680764+notfound07@users.noreply.github.com>
2024-04-30 19:15:40 +0530

    New

--- client/src/user/Reset.jsx
+++ client/src/user/Reset.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { useContext,useState } from 'react';
+import { useContext, useState } from 'react';
 import { RecoveryContext } from "../App";
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 
 const Reset = () => {
-    const { email} = useContext(RecoveryContext);
+    const { email } = useContext(RecoveryContext);
     const [password, setPassword] = useState("");
     const [confirmpassword, setConfirmpassword] = useState("");
-    const [loading, setLoading] = useState(false);
-    const navigate=useNavigate();
+    const [loading] = useState(false);
+    const navigate = useNavigate();
 
 
     const submit = async (e) => {
@@ -18,11 +19,11 @@ const Reset = () => {
             // Make an API request to create a new user
             const response = await axios.post(
                 "http://localhost:3500/user/reset_password",
-                 {
-                email,
-                password,
-                confirmpassword,
-            }
+                {
+                    email,
+                    password,
+                    confirmpassword,
+                }
             );
             if (response.status === 200) {
                 // User reset was successful
@@ -34,21 +35,31 @@ const Reset = () => {
             console.error("Error registering user:", error);
         }
     };
-  return (
-    <div>
-        <form onSubmit={submit}>
-            <label>Email</label>
-            <input defaultValue={email} disabled/>
-            <label>New Password</label>
-            <input type="password" placeholder="Password" id="password" onChange={(e) => {
-            setPassword(e.target.value);}}/>
-            <label>Confirm Password</label>
-            <input type="password" placeholder="confirm password" id="confirmpassword" onChange={(e) => {
-            setConfirmpassword(e.target.value);}}/>
-            <button type="submit"disabled={loading}>{loading ? "Resetting..." : "Reset Password"}</button>
-        </form>
-    </div>
-  )
+    return (
+        <div>
+            <form className="reset-main-box " onSubmit={submit}>
+                <div className='display-reset'>
+                    <div className='form-reset'>
+                        <label>Email</label>
+                        <label>New Password</label>
+                        <label>Confirm Password</label>
+                    </div>
+                    <div className='form-reset'>
+                        <input defaultValue={email} disabled />
+                        <input type="password" placeholder="Password" id="password" onChange={(e) => {
+                            setPassword(e.target.value);
+                        }} />
+                        <input type="password" placeholder="confirm password" id="confirmpassword" onChange={(e) => {
+                            setConfirmpassword(e.target.value);
+                        }} />
+                    </div>
+                </div>
+                <div className='center-reset'>
+                <button className="verify_button" type="submit" disabled={loading}>{loading ? "Resetting..." : "Reset Password"}</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Reset;
